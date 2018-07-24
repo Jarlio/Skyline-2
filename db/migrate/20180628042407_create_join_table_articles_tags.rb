1a60f8185f89988c1d9a6825bad69bca2c9b8c9f
Jarlio <apostol.gabriel90@gmail.com>
2018-07-24 16:45:35 +0300

    complet transfer to mysql

--- db/migrate/20180628042407_create_join_table_articles_tags.rb
+++ db/migrate/20180628042407_create_join_table_articles_tags.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableArticlesTags < ActiveRecord::Migration[5.1]
   def change
     create_join_table :Articles, :Tags do |t|
-      t.integer :article_id
-      t.integer :tag_id
+      # t.integer :article_id
+      # t.integer :tag_id
       t.index [:article_id, :tag_id]
       # t.index [:tag_id, :article_id]
     end
